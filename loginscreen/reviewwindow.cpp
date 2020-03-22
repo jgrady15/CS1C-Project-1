e775e3fef8c6051053e643c8d12756770abdcc20
Eddie <48199805+UnitedPuggs@users.noreply.github.com>
2020-03-22 13:15:09 -0700

    Update reviewwindow.cpp

--- loginscreen/reviewwindow.cpp
+++ loginscreen/reviewwindow.cpp
@@ -1,6 +1,6 @@
 #include "reviewwindow.h"
 #include "ui_reviewwindow.h"
-#include <QMessageBox>
+
 reviewWindow::reviewWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::reviewWindow)
@@ -20,50 +20,80 @@ reviewWindow::~reviewWindow()
     delete ui;
 }
 
-void reviewWindow::makeAnon() {
-    if (this->ui->anonBox->isChecked()) {
+void reviewWindow::makeAnon()
+{
+    if (this->ui->anonBox->isChecked())
+    {
         this->ui->nameLine->setStyleSheet("QLineEdit {background-color:lightgray;}");
         this->ui->nameLine->setText("Anonymous");
         this->ui->nameLine->setReadOnly(true);
-    } else {
+    }
+
+    else
+    {
         this->ui->nameLine->setStyleSheet("QLineEdit {background-color:white;}");
         this->ui->nameLine->setText("");
         this->ui->nameLine->setReadOnly(false);
     }
 }
 
-void reviewWindow::setProduct(int index) {
+void reviewWindow::setProduct(int index)
+{
     this->ui->productLine->setText(this->ui->menuDrop->itemText(index));
 }
 
-void reviewWindow::submitReview() {
+void reviewWindow::submitReview()
+{
+    QSqlQuery query;
     QMessageBox errorOccurred;
+
     errorOccurred.setStyleSheet("QLabel {min-width: 300px;}");
-    if (this->ui->nameLine->text() != "" && this->ui->productLine->text() != "") {
-        errorOccurred.setInformativeText("Review Successfully Submitted!");
-        errorOccurred.setStandardButtons(QMessageBox::Ok);
-        errorOccurred.exec();
-        delete this;
+    if (this->ui->nameLine->text() != "" && this->ui->productLine->text() != "")
+    {
+        QString data = ui->textEdit->toPlainText();
+        QStringList splitData = data.split(QRegExp("[\n]"), QString::SkipEmptyParts);
+
         this->ui->productLabel->setStyleSheet("QLabel {font-weight: normal;}");
         this->ui->nameLabel->setStyleSheet("QLabel {font-weight: normal;}");
-        //Code to put it in an review area?
-    } else if (this->ui->nameLine->text() == "" && this->ui->productLine->text() != "") {
+
+        query.prepare("INSERT OR IGNORE INTO CustomerReviews(CustomerName, ProductName, ReviewNumber, TextReview)"
+                      "VALUES(:name, :item, :rating, :textReview);");
+
+        query.bindValue(":name", ui->nameLine->text());
+        query.bindValue(":item", ui->productLine->text());
+        query.bindValue(":rating", ui->ratingBox->text());
+        query.bindValue(":textReview", splitData);
+
+        if(!query.exec())
+            qDebug() << query.lastError();
+
+        this->close();
+    }
+
+    else if (ui->nameLine->text() == "" && this->ui->productLine->text() != "")
+    {
         errorOccurred.setInformativeText("No name!");
         errorOccurred.setStandardButtons(QMessageBox::Ok);
         errorOccurred.exec();
         this->ui->nameLabel->setStyleSheet("QLabel {font: bold 14px; color: red}");
         this->ui->productLabel->setStyleSheet("QLabel {font-weight: normal;}");
-    } else if (this->ui->nameLine->text() != "" && this->ui->productLine->text() == "") {
+    }
+
+    else if (ui->nameLine->text() != "" && this->ui->productLine->text() == "")
+    {
         errorOccurred.setInformativeText("No product selected!");
         errorOccurred.setStandardButtons(QMessageBox::Ok);
         errorOccurred.exec();
         this->ui->productLabel->setStyleSheet("QLabel {font: bold 14px; color: red}");
         this->ui->nameLabel->setStyleSheet("QLabel {font-weight: normal;}");
-    } else {
-        errorOccurred.setInformativeText("No info provided!");
-        errorOccurred.setStandardButtons(QMessageBox::Ok);
-        errorOccurred.exec();
-        this->ui->nameLabel->setStyleSheet("QLabel {font: bold 14px; color: red}");
-        this->ui->productLabel->setStyleSheet("QLabel {font: bold 14px; color: red}");
     }
+
+//    else if ()
+//    {
+//        errorOccurred.setInformativeText("No info provided!");
+//        errorOccurred.setStandardButtons(QMessageBox::Ok);
+//        errorOccurred.exec();
+//        this->ui->nameLabel->setStyleSheet("QLabel {font: bold 14px; color: red}");
+//        this->ui->productLabel->setStyleSheet("QLabel {font: bold 14px; color: red}");
+//    }
 }
